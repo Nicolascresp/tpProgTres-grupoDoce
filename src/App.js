fa755fc49ad580698873cc5ab1d48a6e888bf817
Nicolascresp <ncrespo@udesa.edu.ar>
2024-09-09 20:48:08 -0300

    Modele la maqueta del proyecto

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Navbar from "./components/navbar/navbar";
+
+
+
+function App() {
+  return (
+    <>
+     < Navbar />
+     <h1>My App in React</h1>
+     <main>
+
+        <h3> Superheroes
+        </h3>
+      
+
+     </main>
+    <footer>
+        <p>Copyright © Dashboard 2022</p>
+    </footer>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,19 @@
+import "./navbar.css"
+
+const Navbar = () => {
+    return(
+    <nav>
+        <ul class="main-nav">
+            <li>elemento menu</li>
+            <li>elemento menu</li>
+            <li>elemento menu</li>
+            <li>elemento menu</li>
+        </ul>
+        <ul class="user">
+            <li>Nombre usuario <img src="./img/user.jpg" alt="" /></li>
+        </ul>
+    </nav>
+    )
+}
+
+export default Navbar
