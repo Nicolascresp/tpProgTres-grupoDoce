229ec360a94ee87f3243713a52aa428c6ab1be22
Nicolascresp <ncrespo@udesa.edu.ar>
2024-09-12 19:06:11 -0300

    Trabajando en favoritos

--- src/pages/Favoritos.js
+++ src/pages/Favoritos.js
@@ -1,51 +1,87 @@
 import React, { Component } from "react";
-import { options } from "../options"; 
-import FavoritosList from "../components/FavoritosList/FavoritosLists";
+import FavoritosList from "../components/FavoritosList/FavoritosList"; // Asegúrate de que la ruta sea correcta
 
 class Favoritos extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      favorites: [], 
-      error: null 
+      favorites: [],
+      error: null
     };
   }
 
   componentDidMount() {
-    this.fetchFavorites(); 
+    this.fetchFavorites();
   }
 
   fetchFavorites = () => {
-    fetch("https://api.themoviedb.org/3/account/21512708/favorite", options)
+    fetch("https://api.themoviedb.org/3/account/null/favorite", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer 8ba8bbe7dfab5ab5da50fbbbaf3e12a2",
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
       .then((response) => response.json())
       .then((data) => this.setState({ favorites: data.results }))
-      .catch((error) => this.setState({ error: 'Error fetching data' }));
+      .catch((e) => this.setState({ error: e.message || "Error fetching data" }));
   };
 
-  handleRemoveFavorite = (id) => {
-    fetch(`https://api.themoviedb.org/3/account/21512708/favorite/${id}`, {
-      method: "DELETE",
+  handleAddFavorite = (id, isMovie) => {
+    fetch("https://api.themoviedb.org/3/account/null/favorite", {
+      method: "POST",
       headers: {
-        ...options.headers, 
-        Authorization: 'Bearer e49603c6bbe4b318b8fbec1efdaa1adb' 
+        Authorization: "Bearer 8ba8bbe7dfab5ab5da50fbbbaf3e12a2",
+        "Content-Type": "application/json"
       },
+      body: JSON.stringify({
+        media_type: isMovie ? "movie" : "tv",
+        media_id: id,
+        favorite: true
+      })
     })
+      .then((response) => response.json())
+      .then(() => {
+        this.fetchFavorites(); // Actualiza la lista de favoritos después de agregar uno nuevo
+      })
+      .catch((e) => this.setState({ error: e.message || "Error adding favorite" }));
+  };
+
+  handleRemoveFavorite = (id, isMovie) => {
+    fetch("https://api.themoviedb.org/3/account/null/favorite", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer 8ba8bbe7dfab5ab5da50fbbbaf3e12a2",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        media_type: isMovie ? "movie" : "tv",
+        media_id: id,
+        favorite: false
+      })
+    })
+      .then((response) => response.json())
       .then(() => {
         this.setState((prevState) => ({
-          favorites: prevState.favorites.filter((fav) => fav.id !== id),
+          favorites: prevState.favorites.filter((fav) => fav.id !== id)
         }));
       })
-      .catch(() => this.setState({ error: 'Error removing favorite' }));
+      .catch((e) => this.setState({ error: e.message || "Error removing favorite" }));
   };
 
   render() {
-    const { favorites } = this.state;
+    const { favorites, error } = this.state;
 
     return (
-      <FavoritosList
-        favorites={favorites}
-        onRemoveFavorite={this.handleRemoveFavorite}
-      />
+      <div>
+        <h2>Mis Favoritos</h2>
+        {error && <p>{error}</p>}
+        <FavoritosList
+          favorites={favorites}
+          onRemoveFavorite={this.handleRemoveFavorite}
+        />
+      </div>
     );
   }
 }
