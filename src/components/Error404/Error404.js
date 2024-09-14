83df59e8ea9a6f9ec51cb00ac0a4606699a66d2f
GregoBonardi <gbonardi@udesa.edu.ar>
2024-09-14 11:47:24 -0300

    pagina error 404

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './Error404.css'; 
 
 class Error404 extends Component {
     render() {
       return (
-        <p>Error 404</p>
-      )
+        <section className="textoError">
+        <img src="/img/Error404.jpg" alt="Error 404" className="imagenError" />
+        <p>Error:</p>
+        <p>La página que estabas buscando no existe</p>
+        <p>Haga click <a href='/' className="linkError">aqui</a> para volver a la página de inicio
+        </p>
+    </section>
+        )
     }
   }
 
