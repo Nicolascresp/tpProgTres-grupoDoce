59a8b4475c7b70b04ff88a199f1e1ae66b085eaf
Bautista Juarez <bjuarez@udesa.edu.ar>
2024-09-20 15:12:44 -0300

    ver mas terminado (punto 4) populares

--- src/pages/Populares.js
+++ src/pages/Populares.js
@@ -1,73 +1,69 @@
 import React, { Component } from 'react';
 import PopularMovies from '../components/PopularMovies/PopularMovies';
 
-
 class Populares extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Populares: [],
+      PopularesPrimeras: [],
+      moviesFiltrado: [], 
+      cargarMas: false,
+      nameValue: ""
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            Populares: [],
-            PopularesPrimeras : [],
-            cargarMas : false,
-            nameValue : ""
-
-        };
-    }
-
-    componentDidMount() {
-        fetch("https://api.themoviedb.org/3/movie/popular?api_key=8ba8bbe7dfab5ab5da50fbbbaf3e12a2")
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data.results);
-                this.setState({
-                    Populares: data.results,
-                    PopularesPrimeras: data.results.slice(0, 10) 
-                    
-                });
-            })
-            .catch((e) => console.log(e));
-    }
-
-    cargarMas () {
+  componentDidMount() {
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=8ba8bbe7dfab5ab5da50fbbbaf3e12a2")
+      .then((response) => response.json())
+      .then((data) => {
         this.setState({
-            cargarMas : true
+          Populares: data.results,
+          PopularesPrimeras: data.results.slice(0, 10),
+          moviesFiltrado: data.results 
+        });
+      })
+      .catch((e) => console.log(e));
+  }
 
-        })
-    }
+  cargarMas() {
+    this.setState({
+      cargarMas: true
+    });
+  }
 
-    handleChangeName(event) {
-        this.setState({
-            nameValue: event.target.value
-        }, () => console.log(this.state.nameValue));
-    }
-    
+  handleFilterChange(event) {
+    const filterValue = event.target.value; 
+    this.setState({
+      nameValue: filterValue,
+      moviesFiltrado: filterValue === "" ? this.state.Populares : this.state.Populares.filter(movie => movie.title.toLowerCase().includes(filterValue))
+    });
+  }
 
-    render() {
+  render() {
+    return (
+      <>
+        <h2 className='titulo'> Peliculas Populares</h2>
 
-        return (
-            <>
+        <form className='form'>
+          <input
+            placeholder='Buscar Pelicula...'
+            className="input"
+            value={this.state.nameValue}
+            onChange={(event) => this.handleFilterChange(event)}
+          />
+        </form>
 
-                <h2 className='titulo'> Peliculas Populares</h2>
-                
-                <form className='form'>
-                    <input className = "input" value = {this.state.nameValue} onChange={(event) => this.handleChangeName(event)}>
+        <section className='home-movies'>
+          <PopularMovies dataPopulares={this.state.cargarMas ? this.state.moviesFiltrado : this.state.moviesFiltrado.slice(0, 10)} />
+        </section>
 
-                    </input>
-
-                    <button type='submit'> Buscar </button>
-                </form>
-
-                <section className='home-movies'>
-                   <PopularMovies dataPopulares={this.state.cargarMas ? this.state.Populares : this.state.PopularesPrimeras} />
-                </section>
-
-                <button className= {this.state.cargarMas ? 'esconder' : 'buttonVermas'} onClick={() => this.cargarMas() } >  {this.state.cargarMas ? '' : 'Cargar Mas...'} </button>
-
-
-            </>
-        );
-    }
+        <button className={this.state.cargarMas ? 'esconder' : 'buttonVermas'} onClick={() => this.cargarMas()}>
+          {this.state.cargarMas ? '' : 'Cargar Mas...'}
+        </button>
+      </>
+    );
+  }
 }
 
 export default Populares;
